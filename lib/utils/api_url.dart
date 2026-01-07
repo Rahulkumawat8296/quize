a91af15da3d71b5c12860310a6da69b208f32e23
hp <rahulkumawat0303@gmail.com>
2026-01-07 06:31:12 +0530

    overly

--- lib/utils/api_url.dart
+++ lib/utils/api_url.dart
@@ -30,3 +30,56 @@ void showToast(String messages) {
       textColor: Colors.white,
       fontSize: 16.0);
 }
+
+
+class Helpers {
+  static OverlayEntry overlayLoader(BuildContext context) {
+    return OverlayEntry(
+      builder: (context) =>  Stack(
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: ModalBarrier(
+              dismissible: false,
+              color: Colors.black,
+            ),
+          ),
+          // Animated loader in the center
+          const Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+              strokeWidth: 4,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  static void hideLoader(OverlayEntry loader) {
+    loader.remove();
+  }
+}
+
+Future<LoginModelApi> LoginRepo({email, context, password}) async{
+  OverlayEntry loader = Helpers.overlayLoader(context);
+  Overlay.of(context).insert(loader);
+  var map= <String ,dynamic> {};
+  map['email'] = email;
+  map ['password'] = password;
+  printDebug(map);
+  http.Response response = await http.post(Uri.parse(ApiUrlClass.loginAuth),
+    headers:  await getAuthHeaderApi(), body: jsonEncode(map),);
+  if(response.statusCode ==200){
+    Helpers.hideLoader(loader);
+    printDebug("Login Api ${response.body}");
+    return LoginModelApi.fromJson(jsonDecode(response.body));
+  }
+  else{
+    Helpers.hideLoader(loader);
+    return LoginModelApi(
+        message: jsonDecode(response.body)['message']);
+
+  }
+
+
+}
